6f1454e66c94bf2d55c3704e88bff55add9e0213
lisheng <740425014@qq.com>
2019-08-26 20:22:31 +0800

    fix:新歌首发

--- src/components/views/homeRecommend/homeRecommend.vue
+++ src/components/views/homeRecommend/homeRecommend.vue
@@ -31,7 +31,7 @@
                             trigger="click"
                             height="300px"
                             :interval="5000"
-                            @change="onchange"
+                            @change="recommendonchange"
                             :autoplay="false"
                         >
                             <el-carousel-item
@@ -83,19 +83,19 @@
                             trigger="click"
                             height="300px"
                             :interval="5000"
-                            @change="onchange"
+                            @change="nwemusiconchange"
                             :autoplay="false"
                         >
                             <el-carousel-item
-                                v-for="(item,index) in pageIndex"
+                                v-for="(item,index) in newpageIndex"
                                 :key="index"
                                 class="carousel_css"
                             >
-                                <div v-for="(item,index) in currentPagemusicdata" :key="index">
+                                <div v-for="(item,index) in newCurrentmusicdata" :key="index">
                                     <div>
                                         <img :src="item.cover" alt srcset width="230" height="230" />
                                     </div>
-                                    <span>{{item.title}}</span>
+                                    <span>{{item.album_name}}</span>
                                 </div>
                             </el-carousel-item>
                         </el-carousel>
@@ -110,36 +110,87 @@
 export default {
     data() {
         return {
-            musiclistdata: [],
-            currentPagemusicdata: [],
+            musiclistdata: [],//歌单推荐的总数居
+            currentPagemusicdata: [],//歌单推荐的分页数据
             currentPage: 1,
             pageSize: 5,
-            pageIndex: 1
+            pageIndex: 1,
+            newmusiclist: [],//新歌首发的总数据
+            newCurrentmusicdata:[],//新歌首发的分页数据
+            newpageSize:9,
+            newcurrentPage:1,
+            newpageIndex:1,
         };
     },
     mounted() {
         this.getmusiclist();
     },
     methods: {
+        /**
+        * @description 歌单推荐的列表
+        * @param {} 
+        * @return 
+        * @author lisheng
+        **/
         getmusiclist() {
             let url = `/musicall/cgi-bin/musicu.fcg?g_tk=1742895503&jsonpCallback=recom6880496722127585&loginUin=2034969809&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&data=%7B%22comm%22%3A%7B%22ct%22%3A24%7D%2C%22category%22%3A%7B%22method%22%3A%22get_hot_category%22%2C%22param%22%3A%7B%22qq%22%3A%22%22%7D%2C%22module%22%3A%22music.web_category_svr%22%7D%2C%22recomPlaylist%22%3A%7B%22method%22%3A%22get_hot_recommend%22%2C%22param%22%3A%7B%22async%22%3A1%2C%22cmd%22%3A2%7D%2C%22module%22%3A%22playlist.HotRecommendServer%22%7D%2C%22playlist%22%3A%7B%22method%22%3A%22get_playlist_by_category%22%2C%22param%22%3A%7B%22id%22%3A8%2C%22curPage%22%3A1%2C%22size%22%3A40%2C%22order%22%3A5%2C%22titleid%22%3A8%7D%2C%22module%22%3A%22playlist.PlayListPlazaServer%22%7D%2C%22new_song%22%3A%7B%22module%22%3A%22QQMusic.MusichallServer%22%2C%22method%22%3A%22GetNewSong%22%2C%22param%22%3A%7B%22type%22%3A0%7D%7D%2C%22new_album%22%3A%7B%22module%22%3A%22music.web_album_library%22%2C%22method%22%3A%22get_album_by_tags%22%2C%22param%22%3A%7B%22area%22%3A7%2C%22company%22%3A-1%2C%22genre%22%3A-1%2C%22type%22%3A-1%2C%22year%22%3A-1%2C%22sort%22%3A2%2C%22get_tags%22%3A1%2C%22sin%22%3A0%2C%22num%22%3A40%2C%22click_albumid%22%3A0%7D%7D%2C%22toplist%22%3A%7B%22module%22%3A%22music.web_toplist_svr%22%2C%22method%22%3A%22get_toplist_index%22%2C%22param%22%3A%7B%7D%7D%2C%22focus%22%3A%7B%22module%22%3A%22QQMusic.MusichallServer%22%2C%22method%22%3A%22GetFocus%22%2C%22param%22%3A%7B%7D%7D%7D`;
             this.$axios.get(url).then(res => {
-                this.musiclistdata = res.data.recomPlaylist.data.v_hot;
                 console.log(res); 
-                this.getCurrentPageData();
+                this.musiclistdata = res.data.recomPlaylist.data.v_hot;//歌单推荐的请求数据
+                this.getCurrentPageData();//
                 let num = parseInt(this.musiclistdata.length / this.pageSize);
                 this.pageIndex = num;
+
+                this.newmusiclist = res.data.new_album.data.list//新歌首发的数据 (album_mid 歌曲ID)(album_name歌曲名字)(singers:[歌手]) 
+                this.getnewmusicCurrentPageData()
+                let newnum = parseInt(this.newmusiclist.length / this.newpageSize)
+                this.newpageIndex = newnum
+                
             });
         },
+        /**
+        * @description 推荐歌单分页设置
+        * @param {} 
+        * @return 
+        * @author lisheng
+        **/
         getCurrentPageData() {
             let begin = (this.currentPage - 1) * this.pageSize;
             let end = this.currentPage * this.pageSize;
             this.currentPagemusicdata = this.musiclistdata.slice(begin, end);
         },
-        onchange(a, i) {
+        /**
+        * @description 推荐歌单轮播改变时候的函数
+        * @param {} 
+        * @return 
+        * @author lisheng
+        **/
+        recommendonchange(a, i) {
             this.currentPage = a + 1;
             this.getCurrentPageData();
-        }
+        },
+        /**
+        * @description 新歌首发的分页设置
+        * @param {} 
+        * @return 
+        * @author lisheng
+        **/
+        getnewmusicCurrentPageData(){
+            let begin = (this.newcurrentPage - 1) * this.newpageSize;
+            let end = this.newcurrentPage * this.newpageSize;
+            this.newCurrentmusicdata = this.newmusiclist.slice(begin,end)
+            console.log(this.newCurrentmusicdata);            
+        },
+        /**
+        * @description 新歌首发轮播改变的数据
+        * @param {} 
+        * @return 
+        * @author lisheng
+        **/
+       nwemusiconchange(a,i){
+           this.newcurrentPage = a + 1
+           this.getnewmusicCurrentPageData()
+       }
     }
 };
 </script>
